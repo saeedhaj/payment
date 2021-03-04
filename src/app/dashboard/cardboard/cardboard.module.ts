0be2edcdd729d2d7ed12a3afa945bf01c21e9891
saeedhaj <saeed.hajiaghzadeh@gmail.com>
2021-03-04 11:34:12 +0330

    add module flex-layout and bootstrap

--- src/app/dashboard/cardboard/cardboard.module.ts
+++ src/app/dashboard/cardboard/cardboard.module.ts
@@ -3,13 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { CardboardRoutingModule } from './cardboard-routing.module';
 import { CardboardComponent } from './cardboard.component';
+ import { FlexLayoutModule } from '@angular/flex-layout';
+ import {MatButtonModule} from '@angular/material/button';
+ import {MatCardModule} from '@angular/material/card';
 
+const MATERIAL_MODULES=[
+  MatButtonModule,
+  MatCardModule
+
+]
 
 @NgModule({
   declarations: [CardboardComponent],
   imports: [
     CommonModule,
-    CardboardRoutingModule
+    CardboardRoutingModule,
+    ...MATERIAL_MODULES,
+     FlexLayoutModule
   ]
 })
 export class CardboardModule { }
